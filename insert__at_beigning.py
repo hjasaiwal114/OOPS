dd256a735de252d9731a88624e1028b03877e5e6
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-02-23 18:40:05 +0000

    this add a node at beigning

---
+++ insert__at_beigning.py
@@ -0,0 +1,42 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+def insertBegin(head, x):
+    temp = Node(x)
+
+    if head != None:
+        head.prev = temp
+    temp.next=head
+    temp.prev = head
+
+    return temp
+
+
+def printDll(head):
+    curr = head
+    while curr != None:
+        print(curr.data, end=" ")
+        curr = curr.next
+    print()
+
+
+head = Node(10)
+temp1 = Node(20)
+temp2 = Node(30)
+
+head.next = temp1
+temp1.prev = head
+
+temp1.next = temp2
+temp2.prev = temp1
+
+printDll(head)
+
+head = insertBegin(head, 40)
+
+printDll(head)
