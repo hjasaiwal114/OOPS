c9e778c255753990c8db0f7162f587b2a24af885
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-02-23 20:26:19 +0000

    reverse a  double linked list

---
+++ reverse_DDl.py
@@ -0,0 +1,48 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+def reverseDll(head):
+    if head == None:
+        return None
+    if head.next == None:
+        return head
+
+    curr = head
+    prev = None
+
+    while curr != None:
+        prev = curr
+        curr.next, curr.prev = curr.prev, curr.next
+        curr = curr.prev
+
+    return prev
+
+
+def printDll(head):
+    curr = head
+    while curr != None:
+        print(curr.data, end=" ")
+        curr = curr.next
+    print()
+
+
+head = Node(10)
+temp1 = Node(20)
+temp2 = Node(30)
+
+head.next = temp1
+temp1.prev = head
+
+temp1.next = temp2
+temp2.prev = temp1
+
+printDll(head)
+
+head = reverseDll(head)
+
+printDll(head)
