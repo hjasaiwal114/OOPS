259b3f67086098fdda3b097b64d48dbbe796250d
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-02-23 17:59:35 +0000

    insert bs in python

--- double_linked_list.py
+++ double_linked_list.py
@@ -1,5 +1,31 @@
-print("Hello World")
 
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+def printDll(head):
+    curr = head
+    while curr != None:
+        print(curr.data, end=" ")
+        curr = curr.next
+    print()
+
+
+head = Node(10)
+temp1 = Node(20)
+temp2 = Node(30)
+
+head.next = temp1
+temp1.prev = head
+
+temp1.next = temp2
+temp2.prev = temp1
+
+printDll(head)
 
     
   
