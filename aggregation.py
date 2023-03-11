10496c36079ec6e9d0f5a7be870c3446a64be53e
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-03-11 12:11:50 +0000

    this the final example of aggregation

--- aggregation.py
+++ aggregation.py
@@ -5,9 +5,9 @@ class Customer:
         self.gender = gender
         self.address = address
 
-    def edit_profile(self,new_name, new_city, new_pin,new_state):
+    def edit_profile(self,new_name = self.name, new_city, new_pin,new_state):
         self.name = new_name
-        self.address.change = address(new_city.new_)
+        self.address.change_address(new_city.new_)
 class Adress:
 
     def __init__(self,city,pincode,state):
@@ -24,4 +24,6 @@ class Adress:
 add = Address("Kolkata", 700156, "WB")
 cust = Customer("Nitish", "Male", add)
 
-"wjshhdh"
+cust.edit_profile("gurgaon", 122011, "haryana")
+
+print(cust.address.pincode)
